33fda9e7ea9f4eb833ea5576909d6f84123bc799
Vihang Patel <vihangpatel@Vihangs-MacBook-Pro.local>
2018-03-31 14:56:41 +0530

    CSS - Add card css

--- movies-react/src/movie-island/Card.js
+++ movies-react/src/movie-island/Card.js
@@ -18,12 +18,12 @@ class Card extends Component {
 					<img className='poster-img' src={`http://image.tmdb.org/t/p/w185/${data.poster_path}`} />
 				</div>
 				<div className='card-detail'>
-					<div>{data.title}</div>
-					<div>{langMap[data.original_language]}</div>
-					<div>{data.vote_count}</div>
-					<div>{data.vote_average}</div>
-					<div>{data.overview}</div>
-					<div>{data.release_data}</div>
+					<div className='card-title'>{data.title}</div>
+					<div className='card-lang'>{langMap[data.original_language]}</div>
+					<div className='card-vote'>{data.vote_count}</div>
+					<div className='card-vote'>{data.vote_average}</div>
+					<div className='card-desc'>{data.overview}</div>
+					<div className='card-release'>{data.release_data}</div>
 				</div>
 			</div>
 		)
