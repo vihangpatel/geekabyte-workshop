209a8a8820c6b61fb3f80efe6510760db03e821e
Manjula Dube <dube.manjula668@gmail.com>
2018-03-31 13:12:21 +0530

    destructor array

---
+++ Byte3/destruct-object.js
@@ -0,0 +1,5 @@
+const instructors = { name1: 'Manjula Dube', name2: 'Vihang Patel' }
+
+// {prop} is short for {prop: prop}
+const { name1, name2 } = instructorObj
+// name1 = 'Manjula Dube'; name2 = 'Vihang Patel'
