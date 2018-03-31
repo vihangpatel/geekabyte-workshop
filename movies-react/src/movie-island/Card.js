893c43b35103bd2bafef6b1026d436de8e59ad92
Vihang Patel <vihangpatel@Vihangs-MacBook-Pro.local>
2018-03-31 14:50:45 +0530

    CSS - Fix style

--- movies-react/src/movie-island/Card.js
+++ movies-react/src/movie-island/Card.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react'
 
 const langMap = {
-  en: 'English'
+	en: 'English'
 }
 
 class Card extends Component {
-  render () {
-    const { data } = this.props
+	render() {
+		const { data } = this.props
 
-    if (!data) {
-      return
-    }
+		if (!data) {
+			return
+		}
 
-    return (
-      <div className='card'>
-        <div className='img-container'>
-          <img className='poster-img' src={`http://image.tmdb.org/t/p/w185/${data.poster_path}`} />
-        </div>
-        <div className='card-detail'>
-          <span>{langMap[data.original_language]}</span>
-          <span>{data.vote_count}</span>
-          <span>{data.vote_average}</span>
-          <span>{data.overview}</span>
-          <span>{data.release_data}</span>
-        </div>
-      </div>
-    )
-  }
+		return (
+			<div className='card'>
+				<div className='img-container'>
+					<img className='poster-img' src={`http://image.tmdb.org/t/p/w185/${data.poster_path}`} />
+				</div>
+				<div className='card-detail'>
+					<div>{data.title}</div>
+					<div>{langMap[data.original_language]}</div>
+					<div>{data.vote_count}</div>
+					<div>{data.vote_average}</div>
+					<div>{data.overview}</div>
+					<div>{data.release_data}</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Card
